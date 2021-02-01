a3610ae8ade29a9b20d0804047a34f9c333da9b2
Gabrielle Anonuevo-Carreon <anonuevg@sheridancollege.ca>
2021-02-01 17:39:45 -0500

    Collision detection and movement controls for player class

--- ILP1/Assets/Scripts/Player.cs
+++ ILP1/Assets/Scripts/Player.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public int move;
+    public int jumpheight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void FixedUpdate()
+    {
+        Vector2 force = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.AddForce(force * move);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            Debug.LogFormat("Collision!");
+        }
+        else if (collision.gameObject.name == "Enemy")
+        {
+            Debug.LogFormat("Ouch!");
+        }
+        else
+        {
+            Debug.Log(collision.gameObject.name);
+        }
     }
 }
